2769ae6c6dad77b42a9781d6499948f587c03478
hepbitkin <mhnaydin505@gmail.com>
2021-04-10 03:18:43 +0300

    redux added and minor bug fixes

---
+++ src/container/Favorites/Favorites.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Favorites</h1>
+        <h2>Harry Potter</h2>
+      </div>
+    );
+  }
+}
+
+export default Favorites;
---
+++ src/store/actions/index.js
@@ -0,0 +1,1 @@
+export { loginAttempt } from "./authenticationActions";
